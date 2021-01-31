76f9f3b75a430693371c645102c2e5affc23cc62
MrVictor42 <guin-hu@hotmail.com>
2021-01-31 15:21:47 -0300

    config jpa in project

---
+++ exercicios-sb/src/main/java/br/com/cod3r/cursojava/exerciciossb/controllers/CalculadoraController.java
@@ -0,0 +1,23 @@
+package br.com.cod3r.cursojava.exerciciossb.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calculadora")
+public class CalculadoraController {
+
+	@GetMapping("/somar/{numero1}/{numero2}")
+	public int somar(@PathVariable int numero1, @PathVariable int numero2) {
+		return numero1 + numero2;
+	}
+	
+	//localhost:8080/calculadora/subtrair?numero2=100&numero1=58
+	@GetMapping("/subtrair")
+	public int subtrair(@RequestParam(name="numero1") int numero1, @RequestParam(name="numero2") int numero2) {
+		return numero1 - numero2;
+	}
+}
